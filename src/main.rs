123c8713e06a687231d1ebac4ac24938b1c75d82
Théophile Wallez <theophile@wallez.net>
2017-11-06 17:57:05 +0100

    Add Map implementation

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,12 @@ pub trait Continuation<V>: 'static {
     /// thus impossible to take the ownership of a `Box<Continuation>` whitout knowing the
     /// underlying type of the `Continuation`.
     fn call_box(self: Box<Self>, runtime: &mut Runtime, value: V);
+
+    /// Creates a new continuation that applies a function to the input value before
+    /// calling `Self`.
+    fn map<F, V2>(self, map: F) -> Map<Self, F> where Self: Sized, F: FnOnce(V2) -> V + 'static {
+        Map { continuation: self, map }
+    }
 }
 
 impl<V, F> Continuation<V> for F where F: FnOnce(&mut Runtime, V) + 'static {
@@ -18,6 +24,21 @@ impl<V, F> Continuation<V> for F where F: FnOnce(&mut Runtime, V) + 'static {
     }
 
     fn call_box(self: Box<Self>, runtime: &mut Runtime, value: V) {
+        (*self).call(runtime, value);
+    }
+}
+
+/// A continuation that applies a function before calling another continuation.
+pub struct Map<C, F> { continuation: C, map: F }
+
+impl<C, F, V1, V2> Continuation<V1> for Map<C, F>
+    where C: Continuation<V2>, F: FnOnce(V1) -> V2 + 'static
+{
+    fn call(self, runtime: &mut Runtime, value: V1) {
+        self.continuation.call(runtime, (self.map)(value));
+    }
+
+    fn call_box(self: Box<Self>, runtime: &mut Runtime, value: V1) {
         (*self).call(runtime, value);
     }
 }
