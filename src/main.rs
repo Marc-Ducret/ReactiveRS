6580b44091379dbe37d8bb8e1e2b4ef08ee871c3
VengeurK <vengeurk@gmail.com>
2017-12-23 15:48:54 +0100

    support for tri color power

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![feature(conservative_impl_trait)]
-#![type_length_limit="8388608"]
+#![type_length_limit="16777216"]
 
 extern crate reactive_rs;
 
--- src/redstone/mod.rs
+++ src/redstone/mod.rs
@@ -3,6 +3,7 @@ extern crate reactive_rs;
 use reactive_rs::reactive::process::*;
 use reactive_rs::reactive::signal::value_signal::*;
 
+use std::ops::{Add, Sub};
 use std::cmp::max;
 use std::sync::{Arc, Mutex};
 
@@ -39,6 +40,46 @@ fn invert_dir(dir: Direction) -> Direction {
     }
 }
 
+#[derive(PartialEq, Clone, Copy)]
+struct Power {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl Add for Power {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        return Power{
+            r: self.r + other.r,
+            g: self.g + other.g,
+            b: self.b + other.b}
+    }
+}
+
+impl Sub for Power {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        return Power{
+            r: self.r - other.r,
+            g: self.g - other.g,
+            b: self.b - other.b}
+    }
+}
+
+fn max_p(p: Power, q: Power) -> Power {
+    Power{
+        r: max(p.r, q.r),
+        g: max(p.r, q.r),
+        b: max(p.r, q.r)}
+}
+
+const ZERO_POWER: Power = Power{r: 0x0, g: 0x0, b: 0x0};
+const ATOMIC_POWER: Power = Power{r: 0x1, g: 0x1, b: 0x1};
+const MAX_POWER: Power = Power{r: 0xF, g: 0xF, b: 0xF};
+
 pub fn redstone_sim() {
     let h = 32;
     let w = 32;
@@ -56,9 +97,9 @@ pub fn redstone_sim() {
 
     let mut power_signal = Vec::new();
     for _ in 0..(w*h) {
-        power_signal.push(ValueSignal::new(0, Box::new(|x: i32, y: i32| max(x, y))));
+        power_signal.push(ValueSignal::new(ZERO_POWER, Box::new(|x: Power, y: Power| max_p(x, y))));
     }
-    let display_signal = ValueSignal::new(vec!(), Box::new(|entries: Vec<(usize, usize, i32)>, entry: (usize, usize, i32)| {
+    let display_signal = ValueSignal::new(vec!(), Box::new(|entries: Vec<(usize, usize, Power)>, entry: (usize, usize, Power)| {
         let mut entries = entries.clone();
         entries.push(entry);
         entries
@@ -66,52 +107,55 @@ pub fn redstone_sim() {
     let power_at = |(x, y): (usize, usize)| power_signal[(x % w) + (y % h) * w].clone();
 
     let redstone_wire_process = |x: usize, y: usize| {
-        let decr = |x: i32| {
+        let decr = |x: Power| {
             use std::thread;
             thread::sleep_ms(100);
-            x-1
+            max_p(x, ATOMIC_POWER) - ATOMIC_POWER
         };
         let continue_loop: LoopStatus<()> = LoopStatus::Continue;
         let input = power_at((x, y));
         let combine_with_pos = move|power| (x, y, power);
-        display_signal.emit(input.emit(
+        let uncombine = move|(x, y, power)| power;
+        input.emit(
             power_at((x + 1, y    )).emit(
                 power_at((x - 1, y    )).emit(
                     power_at((x    , y + 1)).emit(
-                        power_at((x    , y - 1)).emit(input.await().map(decr))))))
-                                .map(combine_with_pos)).then(value(continue_loop)).while_loop()
+                        power_at((x    , y - 1)).emit(
+                            display_signal.emit(
+                                input.await().map(combine_with_pos)).map(uncombine).map(decr))))))
+            .then(value(continue_loop)).while_loop()
     };
 
     let redstone_torch_process = |x: usize, y: usize, dir: Direction| {
         let input = power_at(displace((x, y), invert_dir(dir)));
         let is_powered = |power| {
-            power > 0
+            power != ZERO_POWER
         };
         let mut emit_near = Vec::new();
         for d in vec!(Direction::NORTH, Direction::SOUTH, Direction::EAST, Direction::WEST) {
             if d != invert_dir(dir) {
-                emit_near.push(power_at(displace((x, y), d)).emit(value(0xF)))
+                emit_near.push(power_at(displace((x, y), d)).emit(value(MAX_POWER)))
             }
         }
         let continue_loop: LoopStatus<()> = LoopStatus::Continue;
-        input.emit(value(0)).then(if_else(input.await().map(is_powered), value(()), multi_join(emit_near).then(value(())))).then(value(continue_loop)).while_loop()
+        input.emit(value(ZERO_POWER)).then(if_else(input.await().map(is_powered), value(()), multi_join(emit_near).then(value(())))).then(value(continue_loop)).while_loop()
     };
 
     let display_process = || {
         let mut powers = Vec::new();
         for _ in 0..(w*h) {
-            powers.push(0);
+            powers.push(ZERO_POWER);
         }
-        let powers: Arc<Mutex<Vec<i32>>> = Arc::new(Mutex::new(powers));
+        let powers: Arc<Mutex<Vec<Power>>> = Arc::new(Mutex::new(powers));
         let continue_loop: LoopStatus<()> = LoopStatus::Continue;
         let powers_ref = powers.clone();
-        let read_entries = move|entries: Vec<(usize, usize, i32)>| {
+        let read_entries = move|entries: Vec<(usize, usize, Power)>| {
             let mut powers = powers_ref.lock().unwrap();
             for i in 0..(w*h) {
-                (*powers)[i] = 0;
+                (*powers)[i] = ZERO_POWER;
             }
             for (x, y, power) in entries {
-                (*powers)[x + y * w] = power+1;
+                (*powers)[x + y * w] = power;
             }
         };
         let powers_ref = powers.clone();
@@ -119,7 +163,7 @@ pub fn redstone_sim() {
             let powers = powers_ref.lock().unwrap();
             for y in 0..h {
                 for x in 0..w {
-                    print!("{:X}", (*powers)[x + y * w]);
+                    print!("{:X}", (*powers)[x + y * w].r);
                 }
                 println!();
             }
